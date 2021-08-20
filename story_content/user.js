ba18813ed4972f923060e57dcef35426a4022272
canadianjennifer <canadianjennifer@gmail.com>
2021-08-20 10:44:53 -0400

    test
    test

--- story_content/user.js
+++ story_content/user.js
@@ -2,8 +2,11 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5f0ha8JvUKu":
+      case "5pkGmpPUt44":
         Script1();
+        break;
+      case "6GfdENhR4Zf":
+        Script2();
         break;
   }
 }
@@ -12,22 +15,37 @@ function Script1()
 {
   var player = GetPlayer();
 
-var surname = player.GetVar('surname');
-var name = player.GetVar('name');
-var email = player.GetVar('email');
-var status = player.GetVar('status');
-var score = player.GetVar('score');
+var LearnerName = player.GetVar('LearnerName');
+var LearnerEmail = player.GetVar('LearnerEmail');
+var CorrectAnswer = player.GetVar('CorrectAnswer');
 
 var formData = new FormData();
-formData.append('Surname', surname);
-formData.append('Name', name);
-formData.append('Email', email);
-formData.append('Status', status);
-formData.append('Score', score);
-//formData.append('Some text', 'Here is some text');
+formData.append('LearnerName', LearnerName);
+formData.append('LearnerEmail', LearnerEmail);
+formData.append('CorrectAnswer', CorrectAnswer);
+
 
 var request = new XMLHttpRequest();
-request.open('POST', 'https://script.google.com/macros/s/AKfycbziMxO8jjF1mU_og_XZiJuYM2r485pXZPRkHyXDcIsSY3UDeGaFgpcOOi59oE03JrKVzQ/exec');
+request.open('POST', 'https://script.google.com/macros/s/AKfycbwH9MKCIq73QMCT3eIVeX8T8wGIbAl5XpNV9gRw9J89bwwMuzdfShV2HXUV0jXaRdQ7TA/exec');
+request.send(formData);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var LearnerName = player.GetVar('LearnerName');
+var LearnerEmail = player.GetVar('LearnerEmail');
+var CorrectAnswer = player.GetVar('CorrectAnswer');
+
+var formData = new FormData();
+formData.append('LearnerName', LearnerName);
+formData.append('LearnerEmail', LearnerEmail);
+formData.append('CorrectAnswer', CorrectAnswer);
+
+
+var request = new XMLHttpRequest();
+request.open('POST', 'https://script.google.com/macros/s/AKfycbwH9MKCIq73QMCT3eIVeX8T8wGIbAl5XpNV9gRw9J89bwwMuzdfShV2HXUV0jXaRdQ7TA/exec');
 request.send(formData);
 }
 
